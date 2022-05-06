102a2dc712eb7627e71dec5419ade18b917641d4
Ricardo Guerrero <ricardo.guerrero@economicmodeling.com>
2022-05-06 12:04:01 -0700

    Linting and styling changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
 import './App.css';
+
+const MainApp = styled.div`
+  /* border: 5px solid red; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Square = styled.div`
+  width: 60px;
+  height: 60px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Piece = styled.div`
+  background-color: red;
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  background-color: ${({ piece }) => (piece ? (piece === 'red' ? 'red' : 'black') : 'white')};
+`;
 
 const INITIAL_BOARD_STATE = [
   [null, 'black', null, 'black', null, 'black', null, 'black'],
@@ -10,80 +36,88 @@ const INITIAL_BOARD_STATE = [
   ['red', null, 'red', null, 'red', null, 'red', null],
   [null, 'red', null, 'red', null, 'red', null, 'red'],
   ['red', null, 'red', null, 'red', null, 'red', null]
-]
-
+];
 
 const GameBoard = () => {
   // player 1 can only move red pieces
   // player 2 can only move black pieces
-  // const [currentTurn, setCurrentTurn] = React.setState('player1');
+  const [currentTurn, setCurrentTurn] = React.setState('player1');
   // const [player1Graveyard, setPlayer1Gravetyard] = React.useState([])
   // const [player2Graveyard, setPlayer2Gravetyard] = React.useState([])
   const [moveTo, setMoveTo] = React.useState(undefined);
   const [selectedPiece, setSelectedPiece] = React.useState(undefined);
-  const [boardState, setBoardState] = React.useState(INITIAL_BOARD_STATE)
-  
+  const [boardState, setBoardState] = React.useState(INITIAL_BOARD_STATE);
+
   const movePiece = (rowIndex, columnIndex, piece) => {
-    console.log('hello', !piece)
-    if(!piece) {
-      console.log('nothing to do!')
+    console.log('hello', !piece);
+    if (!piece) {
+      console.log('nothing to do!');
       return;
     }
-    // make sure a piece exist 
+    // make sure a piece exist
     // if()
-    console.log('row ', rowIndex, 'column: ', columnIndex )
+    console.log('row ', rowIndex, 'column: ', columnIndex);
     // make sure a piece currently exists at the given location.
-    
-  }
+  };
 
   const handleSpaceClick = (piece, row, column) => {
-    if(!piece) {
+    if (!piece) {
       console.log('you clicked on an empty space');
       // if there is arlready a selected piece move it here
-      if(selectedPiece) {
-        console.log('place the piece to empty space')
-        console.log('the current board state: ', boardState)
+      if (selectedPiece) {
+        console.log('place the piece to empty space');
+        console.log('the current board state: ', boardState);
+        isValidMove(selectedPiece);
         const boardStateCopy = [...boardState];
         // delete it from previous location
-        boardStateCopy[selectedPiece.row][selectedPiece.column] = null
+        boardStateCopy[selectedPiece.row][selectedPiece.column] = null;
         // add the piece to its new location
-        boardStateCopy[row][column] = selectedPiece.piece
+        boardStateCopy[row][column] = selectedPiece.piece;
 
-
-        setBoardState(boardStateCopy)
-
+        setBoardState(boardStateCopy);
       }
       return;
     }
-    setSelectedPiece({piece, row, column});
-  }
+    setSelectedPiece({ piece, row, column });
+  };
 
-  const canMoveToSquare = () => {
+  const isValidMove = (selectedPieceObject, to) => {
+    const { piece, row: fromRow, column: fromColumn } = selectedPieceObject;
+    console.log('the piece: ', piece, fromRow, fromColumn);
+    if (piece === 'red') {
+      alert('tis a red');
+    } else {
+      alert('its a black');
+    }
+  };
 
-  }
- 
-
-  return <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-    <h1>The GameBoard</h1>
-    <ul>
-      
-{boardState.map((row, rowIndex) => {
-   return <li style={{display: 'flex', border: '1px solid black', width: 'fit-content'}}>
-     {row.map((piece, columnIndex) => {
-     return <div 
-      style={{ border: '1px solid black', width: '60px', height: '60px'}} 
-      onClick={() => {
-        console.log('currently selected piece: ', selectedPiece)
-        handleSpaceClick(piece, rowIndex, columnIndex) 
-      }}
-     >
-       {piece}
-       </div>
-      })}
-   </li>
-  })}
-    </ul>
-  </div>;
+  return (
+    <MainApp style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+      <h1>The GameBoard</h1>
+      <div>
+        <h2>hi</h2>
+      </div>
+      <ul>
+        {boardState.map((row, rowIndex) => {
+          return (
+            <li style={{ display: 'flex', width: 'fit-content' }}>
+              {row.map((piece, columnIndex) => {
+                return (
+                  <Square
+                    onClick={() => {
+                      console.log('currently selected piece: ', selectedPiece);
+                      handleSpaceClick(piece, rowIndex, columnIndex);
+                    }}>
+                    <Piece piece={piece} />
+                  </Square>
+                );
+              })}
+            </li>
+          );
+        })}
+      </ul>
+    </MainApp>
+  );
 };
 
 export default GameBoard;
