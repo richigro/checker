8fef84fe022447d1b7bf70b9da5396b776ddfb8d
ricardo guerrero <richi_gro@hotmail.com>
2022-05-10 01:09:17 -0700

    Add logic to eat pieces

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ const GameBoard = () => {
   const [isPlayerOneTurn, setIsPlayerOneTurn] = React.useState(true);
   const [selectedPiece, setSelectedPiece] = React.useState(undefined);
   const [boardState, setBoardState] = React.useState(INITIAL_BOARD_STATE);
+  const [redPiecesLeft, setRedPiecesLeft] = React.useState(12);
+  const [blackPiecesLeft, setBlackPiecesLeft] = React.useState(12);
 
   const handleMove = (piece, row, column) => {
     if (!piece) {
@@ -54,14 +56,14 @@ const GameBoard = () => {
           setSelectedPiece(undefined);
           setIsPlayerOneTurn((prev) => !prev);
         } else {
-          alert('Invalid move. Try Moving selected piece to another location.');
+          alert('Invalid move.');
           return;
         }
       }
       return;
     }
     // we should only be able to select a piece if you are the correct player
-    console.log('the piece you are trying to select is: ', piece);
+    // console.log('the piece you are trying to select is: ', piece);
     if (isPlayerOneTurn && piece !== 'red') {
       alert('Player 1 can only move red pieces at this time.');
       return;
@@ -69,42 +71,66 @@ const GameBoard = () => {
       alert('Player 2 can only move black pieces at this time.');
       return;
     }
-    console.log('got this far');
     setSelectedPiece({ piece, row, column });
   };
 
   const isValidMove = (selectedPiece, newLocation) => {
     const { piece, row: fromRow, column: fromColumn } = selectedPiece;
     if (piece === 'red') {
-      // what row is it currently sitting in
-      // console.log('the row it is in now: ', fromRow);
-      // console.log('the row it wants to go: ', newLocation.row);
-      // console.log('is the piece moving up? ', isMovingUp(fromRow, newLocation.row));
-      // console.log('is the piece moving Down? ', isMovingDown(fromRow, newLocation.row));
-      // console.log('is the piece moving Right? ', isMovingRight(fromColumn, newLocation.column));
-      // console.log('left? ', isMovingLeft(fromColumn, newLocation.column));
       if (isMovingUp(fromRow, newLocation.row)) {
-        if (
-          isMovingLeft(fromColumn, newLocation.column) ||
-          isMovingRight(fromColumn, newLocation.column)
-        ) {
-          return true;
+        if (Math.abs(fromRow - newLocation.row) === 1) {
+          if (Math.abs(fromColumn - newLocation.column) === 1) {
+            return true;
+          }
+        } else if (Math.abs(fromRow - newLocation.row) === 2) {
+          if (Math.abs(fromColumn - newLocation.column) === 2) {
+            if (isMovingRight(fromColumn, newLocation.column)) {
+              if (boardState[fromRow - 1][fromColumn + 1] === 'black') {
+                const copyOfBoardState = [...boardState];
+                copyOfBoardState[fromRow - 1][fromColumn + 1] = null;
+                setBlackPiecesLeft((prev) => prev - 1);
+                setBoardState(copyOfBoardState);
+                return true;
+              }
+            } else if (isMovingLeft(fromColumn, newLocation.column)) {
+              if (boardState[fromRow - 1][fromColumn - 1] === 'black') {
+                const copyOfBoardState = [...boardState];
+                copyOfBoardState[fromRow - 1][fromColumn - 1] = null;
+                setBlackPiecesLeft((prev) => prev - 1);
+                setBoardState(copyOfBoardState);
+                return true;
+              }
+            }
+          }
         }
       }
       return false;
     } else if (piece === 'black') {
-      // black pieces can only move down in the board
-      // console.log('is the piece moving up? ', isMovingUp(fromRow, newLocation.row));
-      // console.log('is the piece moving Down? ', isMovingDown(fromRow, newLocation.row));
-      // console.log('is the piece moving Right? ', isMovingRight(fromColumn, newLocation.column));
-      // console.log('left? ', isMovingLeft(fromColumn, newLocation.column));
-      // console.log('what');
       if (isMovingDown(fromRow, newLocation.row)) {
-        if (
-          isMovingLeft(fromColumn, newLocation.column) ||
-          isMovingRight(fromColumn, newLocation.column)
-        ) {
-          return true;
+        if (Math.abs(fromRow - newLocation.row) === 1) {
+          if (Math.abs(fromColumn - newLocation.column) === 1) {
+            return true;
+          }
+        } else if (Math.abs(fromRow - newLocation.row) === 2) {
+          if (Math.abs(fromColumn - newLocation.column) === 2) {
+            if (isMovingRight(fromColumn, newLocation.column)) {
+              if (boardState[fromRow + 1][fromColumn + 1] === 'red') {
+                const copyOfBoardState = [...boardState];
+                copyOfBoardState[fromRow + 1][fromColumn + 1] = null;
+                setRedPiecesLeft((prev) => prev - 1);
+                setBoardState(copyOfBoardState);
+                return true;
+              }
+            } else if (isMovingLeft(fromColumn, newLocation.column)) {
+              if (boardState[fromRow + 1][fromColumn - 1] === 'red') {
+                const copyOfBoardState = [...boardState];
+                copyOfBoardState[fromRow + 1][fromColumn - 1] = null;
+                setRedPiecesLeft((prev) => prev - 1);
+                setBoardState(copyOfBoardState);
+                return true;
+              }
+            }
+          }
         }
       }
       return false;
@@ -120,6 +146,8 @@ const GameBoard = () => {
     console.log('restarting game state...');
     setIsPlayerOneTurn(true);
     setSelectedPiece(undefined);
+    setBlackPiecesLeft(12);
+    setRedPiecesLeft(12);
     setBoardState([
       [null, 'black', null, 'black', null, 'black', null, 'black'],
       ['black', null, 'black', null, 'black', null, 'black', null],
@@ -137,6 +165,8 @@ const GameBoard = () => {
       <h1>The GameBoard</h1>
       <div>
         <h2>Current Turn: {isPlayerOneTurn ? 'Player 1' : 'Player 2'}</h2>
+        <div>Black pieces left: {blackPiecesLeft}</div>
+        <div>Red pieces left: {redPiecesLeft}</div>
         <button onClick={handleReset}>Re-start Game</button>
       </div>
       <ul>
